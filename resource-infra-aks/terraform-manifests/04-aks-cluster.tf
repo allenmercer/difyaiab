571469740d54e063e7bd08ae97d651e627deec46
allen.mercer <allen.mercer@ankura.com>
2025-03-12 19:24:13 -0400

    update

--- resource-infra-aks/terraform-manifests/04-aks-cluster.tf
+++ resource-infra-aks/terraform-manifests/04-aks-cluster.tf
@@ -1,9 +1,8 @@
 # Locals Block
 locals {
-    # The location of the Azure Resource Group created for us should be the location for everything.
-    log_workspace_name = "log-${var.project_name}-${var.environment}-${local.location}-${var.project_instance}"
-    aks_cluster_name = "aks-${var.project_name}-${var.environment}-${local.location}-${var.project_instance}"
-    node_resource_group = "nrg-${var.project_name}-${var.environment}-${local.location}-${var.project_instance}"
+    log_workspace_name = "log-${var.project_name}-${var.environment}-${var.location}-${var.project_instance}"
+    aks_cluster_name = "aks-${var.project_name}-${var.environment}-${var.location}-${var.project_instance}"
+    node_resource_group = "nrg-${var.project_name}-${var.environment}-${var.location}-${var.project_instance}"
     nodepool_system_name = "npsystem"
     nodepool_system_labels = {
       "nodepool-type"       = "system"
@@ -15,14 +14,14 @@ locals {
 
 # Azure Kubernetes Service Versions Block
 data "azurerm_kubernetes_service_versions" "current" {
-  location = local.location
+  location = var.location
   include_preview = false
 }
 
 # Azure Log Analytics Workspace Block
 resource "azurerm_log_analytics_workspace" "insights" {
   name                = local.log_workspace_name
-  location            = local.location
+  location            = var.location
   resource_group_name = var.rg_name
   retention_in_days   = var.log_retention_days
 
@@ -33,7 +32,7 @@ resource "azurerm_log_analytics_workspace" "insights" {
 # Get credentials with: az aks get-credentials --admin --name difyai-agent-builder-poc-cluster --resource-group difyai-agent-builder-poc
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = local.aks_cluster_name   
-  location            = local.location
+  location            = var.location
   resource_group_name = var.rg_name
   dns_prefix          = local.aks_cluster_name
   kubernetes_version = data.azurerm_kubernetes_service_versions.current.latest_version
