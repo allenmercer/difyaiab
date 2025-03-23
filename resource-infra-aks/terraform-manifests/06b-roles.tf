149b0977bcb246e1659c3507c92d6702e5504e2f
allen.mercer <allen.mercer@ankura.com>
2025-03-23 10:25:49 -0400

    add aks to pip role

--- resource-infra-aks/terraform-manifests/06b-roles.tf
+++ resource-infra-aks/terraform-manifests/06b-roles.tf
@@ -1,5 +1,7 @@
 # Azure Role Assignment Block for AKS to PIP
 # https://stackoverflow.com/questions/78190342/with-terraform-how-do-i-integrate-a-basic-sku-load-balancer-and-basic-sku-publi
+# assign the kublet id role to the node resource group id itself (which seems counter intuitive)
+# ref: https://learn.microsoft.com/en-us/azure/aks/static-ip?source=recommendations#create-a-service-using-the-static-ip-address
 resource "azurerm_role_assignment" "aks_to_pip_tlb01" {
   description          = "Allows a basic-sku load balancer and basic-sku Public IP integration with an AKS cluster whose load_balancer_sku = basic."
   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
